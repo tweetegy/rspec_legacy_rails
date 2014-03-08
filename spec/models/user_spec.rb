f8eea2676b8a3f450de8875caf53caa96a36ab48
darren_jensen <darren.jensen@gmail.com>
2014-03-08 11:59:10 +0700

    basic rspec setup complete

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe User do
+  it "should be defined" do
+    defined?(User).should_not be_nil
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+require 'capybara/rspec'
+require 'capybara/rails'
+
+RSpec.configure do |config|
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+
+  config.color_enabled = true
+  config.formatter = :documentation
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+end
